e170c6967210caa46a4429fc7f4c07d571d6b487
junior046 <junior.046@hotmail.com>
2021-04-23 19:10:48 -0600

    agregando procedure

--- 08_procedures_create.sql
+++ 08_procedures_create.sql
@@ -5,8 +5,11 @@ CREATE OR REPLACE PROCEDURE proc_person_add (
     p_first_lastname        IN                      person.first_lastname%TYPE,
     p_second_lastname       IN                      person.second_lastname%TYPE
 ) IS
+
     exception_pk EXCEPTION;
+    exception_nn EXCEPTION;
     PRAGMA exception_init ( exception_pk, -1 );
+    PRAGMA exception_init ( exception_nn, -1400 );
 BEGIN
     INSERT INTO person (
         id_person,
@@ -29,10 +32,43 @@ BEGIN
 EXCEPTION
     WHEN exception_pk THEN
         dbms_output.put_line('Error de inserción, ya existe el ID');
-        raise_application_error(-20001, 'YA EXISTE EL ID QUE SE QUIERE AGREGAR');
+        raise_application_error(-20001, 'YA EXISTE EL ID QUE SE QUIERE AGREGAR EN EL REGISTRO DE PERSONAS');
+    WHEN exception_nn THEN
+        dbms_output.put_line('ERROR DE INSERCIÓN, NO PUEDEDEJAR VACÍO UN CAMPO OLIGATORIO.');
+        raise_application_error(-20003, 'NO PUEDEDEJAR VACÍO UN CAMPO OLIGATORIO.');
     WHEN OTHERS THEN
         dbms_output.put_line('CODIGO:' || sqlcode);
         dbms_output.put_line('MENSAJE:' || sqlerrm);
         raise_application_error(-20002, 'HA OCURRIDO UN ERROR, VERIFIQUE LOS DATOS');
 END proc_person_add;
 /
+CREATE OR REPLACE PROCEDURE proc_category_add (
+    p_category      IN              category.category%TYPE,
+    p_description   IN              category.description%TYPE
+) IS
+    exception_pk EXCEPTION;
+    PRAGMA exception_init ( exception_pk, -1 );
+BEGIN
+    INSERT INTO category (
+        id_category,
+        category,
+        description
+    ) VALUES (
+        s_category.NEXTVAL,
+        p_category,
+        p_description
+    );
+
+    dbms_output.put_line('CATEGORÍA AGREGADA CON ÉXITO');
+    COMMIT;
+EXCEPTION
+    WHEN exception_pk THEN
+        dbms_output.put_line('EROOR DE INSERCIÓN, YA EXISTE EL ID');
+        raise_application_error(-20004, 'YA EXISTE EL ID QUE SE QUIERE AGREGAR EN EL REGISTRO DE CATEGORÍA');
+    WHEN OTHERS THEN
+        dbms_output.put_line('CODIGO:' || sqlcode);
+        dbms_output.put_line('MENSAJE:' || sqlerrm);
+        raise_application_error(-20005, 'HA OCURRIDO UN ERROR, VERIFIQUE LOS DATOS');
+END proc_category_add;
+/
+CREATE OR REPLACE PROCEDURE proc_

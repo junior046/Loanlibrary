3bb04a0eeaffa95fe2d641e6c6c44cd47eca7c73
David B <David B@DaveBM.mshome.net>
2021-05-10 09:42:40 -0600

    Cambios mínimos

--- Proyecto01/src/main/java/UserInterface/main.java
+++ Proyecto01/src/main/java/UserInterface/main.java
@@ -251,7 +251,6 @@ public class main extends javax.swing.JFrame {
         confirmExit.setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         confirmExit.setAlwaysOnTop(true);
         confirmExit.setFocusTraversalPolicyProvider(true);
-        confirmExit.setMaximumSize(new java.awt.Dimension(400, 300));
         confirmExit.setMinimumSize(new java.awt.Dimension(400, 300));
         confirmExit.setResizable(false);
         confirmExit.setType(java.awt.Window.Type.UTILITY);
@@ -311,9 +310,7 @@ public class main extends javax.swing.JFrame {
         LogIn_Frame.setAlwaysOnTop(true);
         LogIn_Frame.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         LogIn_Frame.setFocusTraversalPolicyProvider(true);
-        LogIn_Frame.setMaximumSize(new java.awt.Dimension(600, 500));
         LogIn_Frame.setMinimumSize(new java.awt.Dimension(600, 500));
-        LogIn_Frame.setPreferredSize(new java.awt.Dimension(600, 500));
         LogIn_Frame.setResizable(false);
 
         Unchangable_Layered_SignIn.setMaximumSize(new java.awt.Dimension(600, 500));
@@ -368,7 +365,6 @@ public class main extends javax.swing.JFrame {
         LogIn_Panel.setAlignmentY(0.0F);
         LogIn_Panel.setMaximumSize(new java.awt.Dimension(600, 380));
         LogIn_Panel.setMinimumSize(new java.awt.Dimension(600, 380));
-        LogIn_Panel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         LogIn_TextField_UserName.setMaximumSize(new java.awt.Dimension(90, 30));
         LogIn_TextField_UserName.setMinimumSize(new java.awt.Dimension(90, 30));
@@ -378,24 +374,20 @@ public class main extends javax.swing.JFrame {
                 LogIn_TextField_UserNameActionPerformed(evt);
             }
         });
-        LogIn_Panel.add(LogIn_TextField_UserName, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 100, 260, -1));
 
         LogIn_Label_UserName.setFont(new java.awt.Font("Kozuka Mincho Pr6N L", 0, 24)); // NOI18N
         LogIn_Label_UserName.setForeground(new java.awt.Color(255, 255, 255));
         LogIn_Label_UserName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         LogIn_Label_UserName.setText("Username:");
         LogIn_Label_UserName.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        LogIn_Panel.add(LogIn_Label_UserName, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 100, 148, 30));
 
         LogIn_Label_Password.setFont(new java.awt.Font("Kozuka Mincho Pr6N L", 0, 24)); // NOI18N
         LogIn_Label_Password.setForeground(new java.awt.Color(255, 255, 255));
         LogIn_Label_Password.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         LogIn_Label_Password.setText("Password:");
         LogIn_Label_Password.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        LogIn_Panel.add(LogIn_Label_Password, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, 148, 30));
 
         LogIn_PasswordField_Password.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        LogIn_Panel.add(LogIn_PasswordField_Password, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 150, 260, 30));
 
         LogIn_Button_SignUp.setFont(new java.awt.Font("Lucida Sans Unicode", 2, 18)); // NOI18N
         LogIn_Button_SignUp.setText("Sign up ");
@@ -412,7 +404,6 @@ public class main extends javax.swing.JFrame {
                 LogIn_Button_SignUpActionPerformed(evt);
             }
         });
-        LogIn_Panel.add(LogIn_Button_SignUp, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 270, 110, 40));
 
         LogIn_Button_Accept.setBackground(new java.awt.Color(255, 255, 255));
         LogIn_Button_Accept.setFont(new java.awt.Font("Lucida Sans Unicode", 2, 18)); // NOI18N
@@ -431,7 +422,6 @@ public class main extends javax.swing.JFrame {
                 LogIn_Button_AcceptActionPerformed(evt);
             }
         });
-        LogIn_Panel.add(LogIn_Button_Accept, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 280, 70, 30));
 
         LogIn_Button_PasswordRecovery.setBorderPainted(false);
         LogIn_Button_PasswordRecovery.setContentAreaFilled(false);
@@ -442,7 +432,6 @@ public class main extends javax.swing.JFrame {
                 LogIn_Button_PasswordRecoveryActionPerformed(evt);
             }
         });
-        LogIn_Panel.add(LogIn_Button_PasswordRecovery, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, 160, 20));
 
         LogIn_Label_PasswordRecovery.setFont(new java.awt.Font("Kozuka Mincho Pro L", 2, 14)); // NOI18N
         LogIn_Label_PasswordRecovery.setForeground(new java.awt.Color(204, 204, 204));
@@ -450,7 +439,6 @@ public class main extends javax.swing.JFrame {
         LogIn_Label_PasswordRecovery.setToolTipText("");
         LogIn_Label_PasswordRecovery.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         LogIn_Label_PasswordRecovery.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        LogIn_Panel.add(LogIn_Label_PasswordRecovery, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, 170, 20));
 
         LogIn_Button_Accept1.setFont(new java.awt.Font("Lucida Sans Unicode", 2, 18)); // NOI18N
         LogIn_Button_Accept1.setText("Accept");
@@ -467,7 +455,59 @@ public class main extends javax.swing.JFrame {
                 LogIn_Button_Accept1ActionPerformed(evt);
             }
         });
-        LogIn_Panel.add(LogIn_Button_Accept1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 270, 110, 40));
+
+        javax.swing.GroupLayout LogIn_PanelLayout = new javax.swing.GroupLayout(LogIn_Panel);
+        LogIn_Panel.setLayout(LogIn_PanelLayout);
+        LogIn_PanelLayout.setHorizontalGroup(
+            LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                .addGap(270, 270, 270)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LogIn_Button_PasswordRecovery, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogIn_Label_PasswordRecovery, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(LogIn_Button_SignUp, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(120, 120, 120)
+                .addComponent(LogIn_Button_Accept1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(110, 110, 110)
+                .addComponent(LogIn_Button_Accept, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                .addGap(90, 90, 90)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                        .addGap(4, 4, 4)
+                        .addComponent(LogIn_Label_Password, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(LogIn_PasswordField_Password, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                        .addComponent(LogIn_Label_UserName, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(22, 22, 22)
+                        .addComponent(LogIn_TextField_UserName, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        );
+        LogIn_PanelLayout.setVerticalGroup(
+            LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                .addGap(100, 100, 100)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LogIn_Label_UserName, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogIn_TextField_UserName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(17, 17, 17)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LogIn_PasswordField_Password, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogIn_Label_Password, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(10, 10, 10)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LogIn_Button_PasswordRecovery, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogIn_Label_PasswordRecovery, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(60, 60, 60)
+                .addGroup(LogIn_PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LogIn_Button_SignUp, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogIn_Button_Accept1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(LogIn_PanelLayout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(LogIn_Button_Accept, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        );
 
         LogIn_Layered_PanelBase.add(LogIn_Panel);
         LogIn_Panel.setBounds(0, 0, 600, 380);
@@ -722,7 +762,6 @@ public class main extends javax.swing.JFrame {
 
         Unchangable_Layered_Main.setMaximumSize(new java.awt.Dimension(900, 550));
         Unchangable_Layered_Main.setMinimumSize(new java.awt.Dimension(900, 550));
-        Unchangable_Layered_Main.setPreferredSize(new java.awt.Dimension(900, 550));
         Unchangable_Layered_Main.setLayout(new java.awt.BorderLayout());
 
         Main_Panel_Head.setBackground(new java.awt.Color(251, 133, 0));
@@ -735,8 +774,6 @@ public class main extends javax.swing.JFrame {
         Main_label_HeadTitle.setForeground(new java.awt.Color(255, 255, 255));
         Main_label_HeadTitle.setText("ss");
         Main_Panel_Head.add(Main_label_HeadTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 0, 670, 100));
-
-        Main_label_logo.setIcon(new javax.swing.ImageIcon("C:\\Users\\Usuario\\Desktop\\1 SEMESTRE 21\\BASES DE DATOS\\Proyecto 01\\Loanlibrary\\Proyecto01\\src\\main\\java\\UserInterface\\images\\z_04.png")); // NOI18N
         Main_Panel_Head.add(Main_label_logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 210, 120));
 
         Unchangable_Layered_Main.add(Main_Panel_Head, java.awt.BorderLayout.PAGE_START);
@@ -747,7 +784,6 @@ public class main extends javax.swing.JFrame {
 
         Main_Layered_PanelBase.setMaximumSize(new java.awt.Dimension(900, 450));
         Main_Layered_PanelBase.setMinimumSize(new java.awt.Dimension(900, 450));
-        Main_Layered_PanelBase.setPreferredSize(new java.awt.Dimension(900, 450));
 
         Menu_Panel_Main.setBackground(new java.awt.Color(2, 48, 71));
         Menu_Panel_Main.setMaximumSize(new java.awt.Dimension(900, 450));
@@ -1045,7 +1081,6 @@ public class main extends javax.swing.JFrame {
         setLocation(new java.awt.Point(0, 0));
         setMaximumSize(new java.awt.Dimension(0, 0));
         setName("DK"); // NOI18N
-        setPreferredSize(new java.awt.Dimension(0, 0));
         setResizable(false);
         setSize(new java.awt.Dimension(0, 0));
         setType(java.awt.Window.Type.UTILITY);
@@ -1065,7 +1100,7 @@ public class main extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+        hideEveryPanelExceptThis_MAIN(Panel_Prestamos);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void cofirmExit_button_AcceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cofirmExit_button_AcceptActionPerformed
@@ -1084,6 +1119,7 @@ public class main extends javax.swing.JFrame {
 
     private void LogIn_Button_SignUpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogIn_Button_SignUpActionPerformed
         hideEveryPanelExceptThis_LogIn(SignUp_Panel, true);
+        
     }//GEN-LAST:event_LogIn_Button_SignUpActionPerformed
 
     private void LogIn_Button_AcceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogIn_Button_AcceptActionPerformed
